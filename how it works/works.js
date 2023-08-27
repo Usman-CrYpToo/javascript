8b176cacb2cbbc493ce7149e585d136450483dcb
usman <usman.atiq951357@gmail.com>
2023-08-27 19:23:00 +0500

    this is the second commit

--- how it works/works.js
+++ how it works/works.js
@@ -1,11 +1,18 @@
 console.log(this);
-console.log()
+console.log(fullName)
 console.log(names)
+console.log(func)
 
 function names () {
      console.log("usman")
      console.log(this)
 }
+ 
+var func = function () {
+    console.log("this is expression function");
+}
+
+console.log(func)
 
 console.log(names)
 var firstName = "usman";
