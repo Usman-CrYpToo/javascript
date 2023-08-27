32418135f21e9ef6902163e17bf3d6a97402582c
usman <usman.atiq951357@gmail.com>
2023-08-27 16:04:17 +0500

    intail how javascript works

---
+++ how it works/works.js
@@ -0,0 +1,14 @@
+console.log(this);
+console.log()
+console.log(names)
+
+function names () {
+     console.log("usman")
+     console.log(this)
+}
+
+console.log(names)
+var firstName = "usman";
+var lastName = "atique ";
+var fullName = firstName + "  " + lastName;
+console.log(fullName);
